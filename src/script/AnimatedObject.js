8b7a5dbc269c069712ad71d26351f592cceebd82
=Ewgeni <=ewilhelm@stud.hs-emden-leer.de>
2016-04-01 13:10:25 +0200

    ADD: Project1. Interaktiv animierte Scheibe

---
+++ src/script/AnimatedObject.js
@@ -0,0 +1,94 @@
+﻿AnimatedObject = function () {
+    this.spriteImages = [];
+    this.animations = [];
+    this.animationInfo = '';
+    this.currentFrame = 0;
+    this.timerId = 0;
+    this.intervalTimer = 200; // in ms
+    this.animationStatus = false;   //false = stop, true = running
+    this.boardPlace = '';
+}
+
+AnimatedObject.prototype.load = function (boardSlotId) {
+    this.boardPlace = boardSlotId;
+    this.animate();
+}
+
+AnimatedObject.prototype.setAnimation = function (animation) {
+    this.animations.push(animation);
+    return this;
+}
+
+AnimatedObject.prototype.animate = function (extraAnimation) {
+    var self = this;
+    var animation = (typeof extraAnimation === 'undefined') ? 'main' : extraAnimation;
+
+    this.loadAnimation(animation);
+
+    this.setAnimationInfo();
+    this.timerId = setInterval(function () { self.startAnimationCycle(); }, self.intervalTimer);
+    this.animationStatus = true;
+}
+
+AnimatedObject.prototype.loadAnimation = function (animation) {
+
+    var animationName;
+    for (var j = 0; j <= this.animations.length - 1; j++) {
+
+        var objectAnimations = this.animations[j]['animation'];
+        var animationInfos = this.animations[j]['info'];
+
+        for (animationName in objectAnimations) {
+            if (animationName === animation) {
+                this.spriteImages = objectAnimations[animationName];
+                this.animationInfo = animationInfos[animationName];
+            }
+        }
+    }
+
+    this.stopAnimation();//clear interval timer to avoid animation speed multiplication
+}
+
+AnimatedObject.prototype.setAnimationInfo = function () {
+    var infoSlot = this.boardPlace + '-info';
+    document.getElementById(infoSlot).innerHTML = this.animationInfo;
+}
+
+AnimatedObject.prototype.startAnimationCycle = function () {
+
+    if (this.currentFrame > (this.spriteImages.length - 1)) {
+        this.currentFrame = 0;
+    }
+
+    this.loadImage();
+
+    this.currentFrame++;
+}
+
+AnimatedObject.prototype.stopAnimation = function () {
+    clearInterval(this.timerId);
+    this.animationStatus = false;
+}
+
+AnimatedObject.prototype.loadImage = function () {
+
+    if (this.currentFrame > (this.spriteImages.length - 1)) {
+        this.currentFrame = 0;
+    }
+
+    if (this.currentFrame < 0) {
+        this.currentFrame = this.spriteImages.length - 1;
+    }
+
+    var self = this;
+    var dir = '../images/';
+    var imgSrc = dir + this.spriteImages[this.currentFrame];
+    var currentImage = new Image();
+
+    currentImage.onload = function () {
+        var img = document.getElementById(self.boardPlace);
+        img.setAttribute('src', this.src);
+    };
+
+    currentImage.src = imgSrc;
+}
---
+++ src/script/main.js
@@ -0,0 +1,84 @@
+﻿function initSpriteSheetAnimation() {
+    var board = new Board();
+    var slice = new AnimatedObject();
+    var bugsBunny = new AnimatedObject();
+    bugsBunny.intervalTimer = 300;
+
+    slice.setAnimation({
+        'animation': {
+            'main': ['kreis-sprite1.png', 'kreis-sprite2.png',
+                        'kreis-sprite3.png', 'kreis-sprite4.png',
+                        'kreis-sprite5.png', 'kreis-sprite6.png',
+                        'kreis-sprite7.png', 'kreis-sprite8.png',
+                        'kreis-sprite9.png', 'kreis-sprite10.png']
+        },
+        'info': {
+            'main' : '<ul><li>Space = Play/Stop</li><li>l = Schreibe nach links drehen</li><li>r = Schreibe nach rechts drehen</li></ul>'
+        }
+    });
+
+    bugsBunny.setAnimation({
+        'animation': {
+            'main': ['bugs_bunny_muscle1.png', 'bugs_bunny_muscle2.png',
+                        'bugs_bunny_muscle3.png', 'bugs_bunny_muscle4.png',
+                        'bugs_bunny_muscle5.png', 'bugs_bunny_muscle6.png',
+                        'bugs_bunny_muscle7.png', 'bugs_bunny_muscle8.png',
+                        'bugs_bunny_muscle9.png', 'bugs_bunny_muscle10.png',
+                        'bugs_bunny_muscle11.png', 'bugs_bunny_muscle12.png',
+                        'bugs_bunny_muscle13.png', 'bugs_bunny_muscle14.png',
+                        'bugs_bunny_muscle15.png', 'bugs_bunny_muscle16.png',
+                        'bugs_bunny_muscle17.png'],
+
+            'naughty': ['bugs_naughty_animation1.png', 'bugs_naughty_animation2.png',
+                            'bugs_naughty_animation3.png', 'bugs_naughty_animation4.png',
+                            'bugs_naughty_animation5.png']
+        },
+        'info': {
+            'main': 'Pfeiltaste links 2. Animation.<br> [Quelle: http://www.spriters-resource.com/snes/bugsbunnyrabbitrampage/sheet/44964/]',
+            'naughty': 'Pfeiltaste rechts 1. Animation. <br> [Quelle: http://www.spriters-resource.com/snes/bugsbunnyrabbitrampage/sheet/44964/]'
+        }
+    });
+
+    board.bindKeyAnimations = function () {
+
+        window.onkeydown = function (keyEvent) {
+            var keyCode = keyEvent.keyCode;
+
+            switch (keyCode) {
+
+                case 32: //space bar
+
+                    if (!slice.animationStatus) {
+                        slice.animate();
+                    } else {
+                        slice.stopAnimation();
+                    }
+                    break;
+
+                case 37: // left arrow
+                    bugsBunny.animate('naughty');
+                    break;
+
+                case 39: // right arrow
+                    bugsBunny.animate();
+                    break;
+
+                case 76: // l   
+                    slice.currentFrame--;
+                    slice.loadImage();
+                    break;
+
+                case 82: // r   
+                    slice.currentFrame++;
+                    slice.loadImage();
+                    break;
+
+            }
+        }
+    };
+
+    board.appendObject(slice);
+    board.appendObject(bugsBunny);
+    board.create();
+}
+
